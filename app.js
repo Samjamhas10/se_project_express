d608d1c72d8be655f0752f618a7a65e21cb61683
SamSam Hashi <sammymadobe@gmail.com>
2025-05-25 18:19:53 -0500

    Create a database

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express"); // import Express library used to build our web server
+const mongoose = require("mongoose");
 
 const app = express(); // create an instance of an Express application
-
 const { PORT = 3001 } = process.env; // get port number or use 3001 as default
 
-app.listen(PORT, () => { // start the server
-  console.log(`App listening at port ${PORT}`);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.listen(PORT, () => {
+  // start the server
+  console.log(`Server is running on port ${PORT}`);
 });
