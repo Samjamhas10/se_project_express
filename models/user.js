3b75ea293d542b402b13ae3e697949a8c104bc56
SamSam Hashi <sammymadobe@gmail.com>
2025-05-25 19:03:42 -0500

    Fix: corrected changes in wrong file

--- models/user.js
+++ models/user.js
@@ -1,5 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // import the mongoose library to interact with MongoDB
+const validator = require("validator"); // import the validator library to validate values like URLs
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({ // define a user schema
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: [true, "The avatar field is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL", // message shown if URL is invalid
+    },
+  },
+});
 
-modules.export = mongoose.model("user", userSchema);
+module.export = mongoose.model("user", userSchema);
